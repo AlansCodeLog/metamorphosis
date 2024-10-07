c9c2351d62ae16e148d5bfe74d1066d4f57058d7
Alan North <alanscodelog@gmail.com>
2024-10-07 19:52:08 -0300

    deps: updated to new eslint-config

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,11 @@
 // advanced
-import { allFileTypes, tsEslintConfig, vueConfig } from "@alanscodelog/eslint-config"
+import { tsEslintConfig, typescriptConfig } from "@alanscodelog/eslint-config"
 export default tsEslintConfig( // this is just a re-export of tsEslint.config
-	// https://github.com/AlansCodeLog/eslint-config
-	...vueConfig,
-	{
-		files: [`**/*.{${allFileTypes.join(",")}}`],
-		languageOptions: {
-			parserOptions: {
-				// eslint-disable-next-line camelcase
-				EXPERIMENTAL_useProjectService: false,
-				project: "./tsconfig.eslint.json",
-			},
-		},
-
-		ignores: [
-			// ...
-		],
-	},
+	...typescriptConfig,
+	// {
+	// 	ignores: [
+	// 	],
+	// }
 	// RULE LINKS
 	// Eslint: https://eslint.org/docs/rules/
 	// Typescript: https://typescript-eslint.io/rules/
